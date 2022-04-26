98edc77f7bc051aceb5deb4a64630d696400d466
Noah Fehr <noah.fehr@yahoo.com>
2022-04-26 12:51:47 -0500

    Update thread.c

--- kernel/thread.c
+++ kernel/thread.c
@@ -1,13 +1,5 @@
 #include "thread.h"
 
-uintptr_t func; //ptr to function to run
-    uint32_t thread_id;
-    uint8_t state; // running, waiting, ready, dying
-    char name[8]; 
-    uint8_t *stack; 
-    unsigned buffer;
-    void * args; // arguments for the function
-
 int thread_create(thread_t * memory, const char * name, uintptr_t func, void * args) {
     memory->func = func;
     memory->thread_id = 1; // TODO update this so this is different for each thread
